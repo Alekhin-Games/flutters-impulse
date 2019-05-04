e9170e4bb7ad921143674e21d532267239c3bb9e
Japa Alekhin Llemos <jap@alekhin.io>
2019-05-05 02:32:42 +0800

    added producer text

--- lib/components/dialog.dart
+++ lib/components/dialog.dart
@@ -15,6 +15,7 @@ class Dialog extends GameObject {
   TextComponent scoreText;
   TextComponent buttonText;
   TextComponent creditsText;
+  TextComponent producerText;
 
   Dialog(this.game) : super(game) {
     double paddingX = 0.1;
@@ -32,26 +33,55 @@ class Dialog extends GameObject {
     double buttonHeight = buttonWidth / 2;
     double padding = game.tileSize;
 
-    playButton = Rect.fromLTWH((game.viewport.width / 2) - buttonWidth / 2,
-        wrapper.center.dy - 12.5, buttonWidth, buttonHeight);
+    playButton = Rect.fromLTWH((game.viewport.width / 2) - buttonWidth / 2, wrapper.center.dy - 12.5, buttonWidth, buttonHeight);
     playButtonBox = RRect.fromRectAndRadius(playButton, Radius.circular(4));
 
     paint = Paint();
 
-    titleText =
-        TextComponent(game, 'Game Over!', 45.0, rectTop + padding, 0xff3D4852);
-    scoreText =
-        TextComponent(game, '', 30.0, playButton.top - padding / 2, 0xff606F7B);
+    titleText = TextComponent(
+      game,
+      'Game Over!',
+      45.0,
+      rectTop + padding,
+      0xff3D4852,
+    );
+    children.add(titleText);
+
+    scoreText = TextComponent(
+      game,
+      '',
+      30.0,
+      playButton.top - padding / 2,
+      0xff606F7B,
+    );
+    children.add(scoreText);
 
     buttonText = TextComponent(
-        game, 'Play again', 25.0, playButton.center.dy, 0xfffafafa);
-    creditsText = TextComponent(game, 'Made with ❤ by @impulse', 20.0,
-        wrapper.bottom - padding, 0xff6CB2EB);
-
-    children.add(titleText);
-    children.add(scoreText);
+      game,
+      'Play again',
+      25.0,
+      playButton.center.dy,
+      0xfffafafa,
+    );
     children.add(buttonText);
+
+    creditsText = TextComponent(
+      game,
+      'Made with ❤ by @impulse',
+      20.0,
+      wrapper.bottom - padding,
+      0xff6CB2EB,
+    );
     children.add(creditsText);
+
+    producerText = TextComponent(
+      game,
+      'Published by Alekhin Games',
+      20.0,
+      wrapper.bottom - (padding * .6),
+      0xff6cb2eb,
+    );
+    children.add(producerText);
   }
 
   @override
